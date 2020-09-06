5dd7a2e2897d0e816a40e7004a8ca228090b7f3d
Irfan Jauhari <kangrykong123@gmail.com>
2020-09-06 15:06:30 +0700

    FIx port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const log = require('morgan');
